ccfbac39b6a2bc8be56430a1a57f07f589502126
Dan Kiss <flowfast47@gmail.com>
2017-08-15 21:29:31 -0700

    add basic frame of js ad html to make leap year

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var leapYear = function(year) {
+      if (year % 400 ===0) {
+        return true;
+      } else if (year % 100 === 0) {
+        return false;
+      } else if (year % 4 === 0)
+      { return true;
+      } else {
+      return false;
+      }
+    };
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
